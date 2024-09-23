4fee1357e45ef893555d11e4286a9b39ad1c3845
joaovitor101 <joao.kusaka27@gmail.com>
2024-09-23 03:32:49 -0300

    Versão 0.0.1

--- controllers/ClientesController.js
+++ controllers/ClientesController.js
@@ -1,7 +1,7 @@
 import express from 'express'
 const router = express.Router()
 
-import Cliente from '../models/Cliente.js'
+// import Cliente from '../models/Cliente.js'
 // ROTA CLIENTES
 router.get("/clientes", function(req,res){
     const clientes = [
--- controllers/ProdutosController.js
+++ controllers/ProdutosController.js
@@ -1,17 +1,17 @@
-import express from 'express'
-const router = express.Router()
+import express from 'express';
+const router = express.Router();
 
 // ROTA PRODUTOS
-router.get("/produtos", function(req,res){
+router.get("/produtos", function(req, res) {
     const produtos = [
-        {nome: "Celular Motorola E22", preco: 1200, categoria: "Eletroportáteis"},
-        {nome: "Tablet Samsung", preco: 900, categoria: "Eletrônicos"},
-        {nome: "Notebook Lenovo", preco: 3200, categoria: "Computadores"},
-        {nome: "Fone Bluetooth", preco: 150, categoria: "Periféricos"}
-    ]
+        {imagem: "../imgs/shorts.avif", nome: "Shorts Nike Treino 2024", preco: 1200, categoria: "Eletroportáteis"},
+        {imagem: "../imgs/celtics.avif", nome: "Camiseta Nike Boston Celtics", preco: 900, categoria: "Eletrônicos"},
+        {imagem: "../imgs/corta_vento.avif", nome: "Capa de Chuva Nike Corinthians 2024", preco: 3200, categoria: "Computadores"},
+        {imagem: "../imgs/regata_nike.avif", nome: "Fone Bluetooth", preco: 150, categoria: "Periféricos"}
+    ];
     res.render("produtos", {
         produtos: produtos
-    })
-})
+    });
+});
 
-export default router
+export default router;

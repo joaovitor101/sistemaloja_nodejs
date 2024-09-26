ad555b98bcb80172c2cf1935dfb2139136167fed
joaovitor101 <joao.kusaka27@gmail.com>
2024-09-25 23:17:02 -0300

    Versão 0.0.2

--- controllers/ProdutosController.js
+++ controllers/ProdutosController.js
@@ -4,10 +4,14 @@ const router = express.Router();
 // ROTA PRODUTOS
 router.get("/produtos", function(req, res) {
     const produtos = [
-        {imagem: "../imgs/shorts.avif", nome: "Shorts Nike Treino 2024", preco: 1200, categoria: "Eletroportáteis"},
-        {imagem: "../imgs/celtics.avif", nome: "Camiseta Nike Boston Celtics", preco: 900, categoria: "Eletrônicos"},
-        {imagem: "../imgs/corta_vento.avif", nome: "Capa de Chuva Nike Corinthians 2024", preco: 3200, categoria: "Computadores"},
-        {imagem: "../imgs/regata_nike.avif", nome: "Fone Bluetooth", preco: 150, categoria: "Periféricos"}
+        {imagem: "../imgs/shorts.avif", nome: "Shorts Nike Treino 2024", preco: 1200, categoria: "Roupa"},
+        {imagem: "../imgs/celtics.avif", nome: "Camiseta Nike Boston Celtics", preco: 900, categoria: "Roupa"},
+        {imagem: "../imgs/corta_vento.avif", nome: "Capa de Chuva Nike Corinthians 2024", preco: 3200, categoria: "Roupa"},
+        {imagem: "../imgs/regata_nike.avif", nome: "Regata Brooklyn 2024", preco: 150, categoria: "Roupa"},
+        {imagem: "../imgs/corintia.avif", nome: "Camisa Corinthians 2024", preco: 350, categoria: "Roupa"},
+        {imagem: "../imgs/camisa01.avif", nome: "Camisa Nike NBA 2024", preco: 150, categoria: "Roupa"},
+        {imagem: "../imgs/barsa.avif", nome: "Camisa Nike Barcelone", preco: 150, categoria: "Roupa"},
+        {imagem: "../imgs/camisa02.avif", nome: "Camisa Brooklyn Net Masc.", preco: 150, categoria: "Roupa"},
     ];
     res.render("produtos", {
         produtos: produtos
